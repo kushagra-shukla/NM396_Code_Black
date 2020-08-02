941bbf525151a306d6ead8e1c1fad2528c699a18
kanishk <kanishksinghal2@gmail.com>
2020-08-02 10:08:49 +0530

    conflict resolved

--- code_black/code_black/settings.py
+++ code_black/code_black/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = 'ac52dt74qk1%x=z%%*&u%83*2hlf$xbv#k#hc_tvm@4r_w8q)t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = ['7c56e3e77aa1.ngrok.io']
-=======
-ALLOWED_HOSTS = ['d6f7558e07ea.ngrok.io']
->>>>>>> 47de161169d4638d161bcd15b872792787b10631
 
 
 # Application definition
