fc0c7bb3478d653f50472a7eda8f58b7b9e322bf
kanishk <kanishksinghal2@gmail.com>
2020-08-03 15:18:31 +0530

    Postgres database added

--- code_black/code_black/settings.py
+++ code_black/code_black/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'code_black.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "code_black_db",
+        'USER': "team_code_black",
+        'PASSWORD': "mypass",
+        'HOST': "127.0.0.1",
+        'PORT': "5432"
+    },
 }
 
 # Redis settings
